b4059eea421293b6dcf14e9611fb13f457250c90
Antony Holmes <antony@antonybholmes.com>
2023-01-04 17:32:58 -0500

    Bug fixes and updates.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,6 +10,7 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
+    `gatsby-plugin-pnpm`,
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
