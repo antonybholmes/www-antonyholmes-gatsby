ca6441a6a5f1669251c5348d4975a7652d09858b
Antony Holmes <antony@antonybholmes.com>
2023-01-05 22:55:59 -0500

    Bug fixes and updates.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,30 +5,37 @@ import ContentDiv from "./content-div"
 import WhiteLink from "./link/white-link"
 
 const Footer = () => (
-  <footer className="pt-16 text-sm">
+  <footer className="py-16 text-xs">
     <ContentDiv>
       <></>
 
-      <ul className="flex flex-row justify-center gap-x-8 rounded-lg bg-slate-100 p-4">
-        {INFO_LINKS.map(
-          (link: { name: string; url: string }, index: number) => (
-            <li key={index}>
-              <WhiteLink
-                href={link.url}
-                ariaLabel={`View ${link.name}`}
-                underline={true}
-              >
-                {link.name}
-              </WhiteLink>
-            </li>
-          )
-        )}
+      <ul className="flex flex-col justify-between gap-y-4 border-t border-slate-300 pt-4 md:flex-row">
+        <li>{getCopyright()}</li>
+        <li>
+          <ul className="flex flex-row gap-y-4 gap-x-8">
+            {INFO_LINKS.map(
+              (link: { name: string; url: string }, index: number) => (
+                <li key={index}>
+                  <WhiteLink
+                    href={link.url}
+                    ariaLabel={`View ${link.name}`}
+                    underline={true}
+                  >
+                    {link.name}
+                  </WhiteLink>
+                </li>
+              )
+            )}
+          </ul>
+        </li>
+        {/* <ul className="flex flex-row justify-center gap-x-8">
+          <li>{getCopyright()}</li>
+        </ul> */}
       </ul>
-
       <></>
     </ContentDiv>
 
-    <ContentDiv className="py-8">
+    {/* <ContentDiv className="py-8">
       <></>
       <>
         <ul className="flex flex-row justify-center gap-x-8">
@@ -36,7 +43,7 @@ const Footer = () => (
         </ul>
       </>
       <></>
-    </ContentDiv>
+    </ContentDiv> */}
   </footer>
 )
 
